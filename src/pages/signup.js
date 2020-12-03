7b3b3d4f953517c1ccd053faf8d32c15bd1dc194
Stephen Axtell <axtell.stephen@gmail.com>
2020-12-03 14:15:47 +0200

    added signup page plus added code for firebase to accept new users

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { FirebaseContext } from '../context/firebase';
+import { HeaderContainer } from '../containers/Header';
+import { FooterContainer } from '../containers/Footer';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function Signup() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  // state
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // validation
+  const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+  const handleSignup = event => {
+    event.preventDefault();
+
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+        // firebase method
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE); // once signed up , go to browse
+          })
+      )
+      .catch((error) => {
+        setFirstName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
+  };
+  
   return (
-    <p>Hello from signup</p>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" title="Sign Up">
+              Sign Up
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
