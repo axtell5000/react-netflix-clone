4707e868d473ffcae6ddf7ffe3f8ecb3c804195a
Stephen Axtell <axtell.stephen@gmail.com>
2020-12-04 14:38:03 +0200

    added custom hook to fetch data from firebase

---
+++ src/hooks/use-content.js
@@ -0,0 +1,29 @@
+import { useEffect, useState, useContext } from 'react';
+
+import { FirebaseContext } from '../context/firebase';
+
+export default function useContent(target) {
+  // some state and pulling firebase object from context
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => { // get data, then take a snapshot, then map over it
+        const allContent = snapshot.docs.map((contentObj) => ({
+          ...contentObj.data(),
+          docId: contentObj.id,
+        }));
+
+        setContent(allContent);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+  // dynamic property name [target]
+  return { [target]: content };
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import { useContent } from '../hooks';
+
 export default function Browse() {
+  // need series and film data
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(films);   
+  // need sliders
+  // pass them to the browse container
   return (
     <p>Hello from browse</p>
   );
