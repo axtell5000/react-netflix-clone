aca03d1430de0c7da99a33791a8120486ce55970
Stephen Axtell <axtell.stephen@gmail.com>
2020-12-10 16:39:55 +0200

    adding card structure and feature area structure

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -6,7 +6,7 @@ import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 // component
-import { Card, Header, Loading } from '../components';
+import { Card, Header, Loading, Player } from '../components';
 
 export function BrowseContainer({ slides }) {
   // state
@@ -80,6 +80,22 @@ export function BrowseContainer({ slides }) {
           slideRows.map((slideItem) => (
             <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
               <Card.Title>{slideItem.title}</Card.Title>
+              <Card.Entities>
+                {slideItem.data.map((item) => (
+                  <Card.Item key={item.docId} item={item}>
+                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                    <Card.Meta>
+                      <Card.SubTitle>{item.title}</Card.SubTitle>
+                      <Card.Text>{item.description}</Card.Text>
+                    </Card.Meta>
+                  </Card.Item>
+                ))}
+              </Card.Entities>
+
+              <Card.Feature category={category}>
+                <p>hello</p>
+              </Card.Feature>
+
             </Card>
           ))
         }
