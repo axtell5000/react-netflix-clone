b8971c7b901fc8b902c7edb95df5e91eaba923bc
Stephen Axtell <axtell.stephen@gmail.com>
2020-12-17 10:56:21 +0200

    setting up tests, wrote ui tests for accordion, footer and player

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -21,7 +21,7 @@ Player.Video = function PlayerVideo({ src, ...restProps }) {
   // createPortal allows us to put something on the DOM, you do have to say where it goes
   return showPlayer
     ? ReactDOM.createPortal(
-        <Overlay onClick={() => setShowPlayer(false)} {...restProps}>
+        <Overlay onClick={() => setShowPlayer(false)} {...restProps} data-testid="player">
           <Inner>
             <video id="netflix-player" controls>
               <source src={src} type="video/mp4" />
